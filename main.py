2e25805fb9f4b54fd4ef0c59c9af950030bc2f6f
Adamm1532 <madon@student.agh.edu.pl>
2024-03-19 10:04:40 +0100

    Test git

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('test')
+import tkinter as tk
+import customtkinter as ctk
+
+window = tk.Tk()
+window.geometry("400x400")
+window.configure(bg="#505cc2")
+label = tk.Label(text="Hello, Tkinter!",
+                 fg="black",
+                 bg="#34A2FE",
+                 width=10,
+                 height=5, font=("Arial", 20))
+label.pack()
+window.mainloop()
+
+#root = ctk.CTk()
+#root.geometry("400x400")
+#button = ctk.CTkButton(root, text="Click me!")
+#button.place(relx=0.5, rely=0.5, anchor="center")
+#root.mainloop()
