793f6285d72a694bb6410bd771de82d40902a130
Domicjan <doriansraga@wp.pl>
2024-03-29 12:33:22 +0100

    added setting png

--- Glowna.py
+++ Glowna.py
@@ -16,7 +16,18 @@ class Glowna(ctk.CTk):
                                             border_color=glb.color_background,
                                             width=420, height=740)
         self.backgroundFrame.place(relx=self.Frame_x, rely=0, anchor="nw")
+        
+        self.label = ctk.CTkLabel(self.backgroundFrame, text="Witaj w Banku", font=("Arial", 20))
+        self.label.place(relx=0.5, rely=0.1, anchor="center")
 
+        self.Button_Login = ctk.CTkButton(self.backgroundFrame,
+                                        text="",
+                                        fg_color=glb.color_magenta,
+                                        hover_color=glb.color_blue,
+                                        width=120, height=50,
+                                        corner_radius=10,
+                                        command=self.clicked)
+        self.Button_Login.place(relx=self.Bank_x, rely=0.7, anchor="center")
 
 
 
